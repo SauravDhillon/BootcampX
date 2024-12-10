8dbe4dc7a9ccb7dc83fdb14b6afa1b8539ebab64
Saurav Dhillon <souravdhillon010@gmail.com>
2024-12-10 02:57:40 -0500

    Entered new queries on JOIN of tables

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,6 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM students JOIN cohorts
+ON cohort_id = cohorts.id
+JOIN assignment_submissions 
+ON student_id = students.id
+WHERE cohorts.name = 'FEB12';
